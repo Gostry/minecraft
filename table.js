e26fdebbbf39f3f78ac4f9ae6dfff8134dcba3ea
Gostry <cobtahacademy21@gmail.com>
2025-10-23 16:59:29 +0300

    table2

---
+++ table.js
@@ -0,0 +1,18 @@
+function button1() {
+    document.getElementById("day1").style.display = "inline-block";
+}
+function button2() {
+    document.getElementById("day2").style.display = "inline-block";
+}
+function button3() {
+    document.getElementById("day3").style.display = "inline-block";
+}
+function button4() {
+    document.getElementById("day4").style.display = "inline-block";
+}
+function button5() {
+    document.getElementById("day5").style.display = "inline-block";
+}
+function button6() {
+    document.getElementById("day6").style.display = "inline-block";
+}
