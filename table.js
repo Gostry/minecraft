2d74a7187519b1e7bed14c31777e034706b148f5
Gostry <cobtahacademy21@gmail.com>
2025-10-28 16:55:31 +0200

    ulcard

--- table.js
+++ table.js
@@ -1,18 +1,56 @@
+function button0() {
+    document.getElementById("day1").style.display = "inline-block";
+    document.getElementById("day2").style.display = "inline-block";
+    document.getElementById("day3").style.display = "inline-block";
+    document.getElementById("day4").style.display = "inline-block";
+    document.getElementById("day5").style.display = "inline-block";
+    document.getElementById("day6").style.display = "inline-block";
+}
 function button1() {
     document.getElementById("day1").style.display = "inline-block";
+    document.getElementById("day2").style.display = "none";
+    document.getElementById("day3").style.display = "none";
+    document.getElementById("day4").style.display = "none";
+    document.getElementById("day5").style.display = "none";
+    document.getElementById("day6").style.display = "none";
 }
 function button2() {
+    document.getElementById("day1").style.display = "none";
     document.getElementById("day2").style.display = "inline-block";
+    document.getElementById("day3").style.display = "none";
+    document.getElementById("day4").style.display = "none";
+    document.getElementById("day5").style.display = "none";
+    document.getElementById("day6").style.display = "none";
 }
 function button3() {
+    document.getElementById("day1").style.display = "none";
+    document.getElementById("day2").style.display = "none";
     document.getElementById("day3").style.display = "inline-block";
+    document.getElementById("day4").style.display = "none";
+    document.getElementById("day5").style.display = "none";
+    document.getElementById("day6").style.display = "none";
 }
 function button4() {
+    document.getElementById("day1").style.display = "none";
+    document.getElementById("day2").style.display = "none";
+    document.getElementById("day3").style.display = "none";
     document.getElementById("day4").style.display = "inline-block";
+    document.getElementById("day5").style.display = "none";
+    document.getElementById("day6").style.display = "none";
 }
 function button5() {
+    document.getElementById("day1").style.display = "none";
+    document.getElementById("day2").style.display = "none";
+    document.getElementById("day3").style.display = "none";
+    document.getElementById("day4").style.display = "none";
     document.getElementById("day5").style.display = "inline-block";
+    document.getElementById("day6").style.display = "none";
 }
 function button6() {
+    document.getElementById("day1").style.display = "none";
+    document.getElementById("day2").style.display = "none";
+    document.getElementById("day3").style.display = "none";
+    document.getElementById("day4").style.display = "none";
+    document.getElementById("day5").style.display = "none";
     document.getElementById("day6").style.display = "inline-block";
 }
